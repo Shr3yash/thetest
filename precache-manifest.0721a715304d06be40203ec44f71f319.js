be15a2cd4ceb14789efc84f2b340109dc594dd6f
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2022-01-03 09:50:03 +0530

    Add files via upload

---
+++ precache-manifest.0721a715304d06be40203ec44f71f319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81da6e8f1ee89d06b131",
+    "url": "/static/css/main.a181637e.chunk.css"
+  },
+  {
+    "revision": "81da6e8f1ee89d06b131",
+    "url": "/static/js/main.a5cb982b.chunk.js"
+  },
+  {
+    "revision": "511aa671d02c1f3f36f0",
+    "url": "/static/js/runtime~main.a2c5e74b.js"
+  },
+  {
+    "revision": "ee7f37ea120d5f770ab3",
+    "url": "/static/js/2.18feb4ae.chunk.js"
+  },
+  {
+    "revision": "e04ab51baa6a52428e47",
+    "url": "/static/js/3.bfe9f187.chunk.js"
+  },
+  {
+    "revision": "f31006b6db31fc7d38d734cd7a8d05d9",
+    "url": "/index.html"
+  }
+];
